5478b18480f6e90a19ca007ceb2c67533cc79047
hxuu <133578197+hxuu@users.noreply.github.com>
2024-12-18 21:17:05 +0100

    add prufer decoding algorithm

--- algorithms/prufer.py
+++ algorithms/prufer.py
@@ -10,8 +10,10 @@ Algorithme :
 - Critère d'arrêt :
      Il ne reste que deux sommets dans l'arbre A.
 """
+import pprint
 from parser import parse_graph_data
-from collections import defaultdict, deque
+from collections import deque
+
 
 def prufer_encoding(tree_path):
     adjacency_list, _, _, headers = parse_graph_data(tree_path)
@@ -41,9 +43,35 @@ def prufer_encoding(tree_path):
     return P, sorted(headers)
 
 
+def prufer_decoding(P, sorted_headers):
+    adjacency_list = {element: [] for element in sorted_headers}
+    # print(adjacency_list)
+
+    while P or len(sorted_headers) > 2:
+        # this can probably be more effecient but meh~ Idc
+        least_element = [x for x in sorted_headers if x not in P][0]
+
+        # create an edge between least_element and P[0]
+        adjacency_list[least_element].append((P[0], None))
+        adjacency_list[P[0]].append((least_element, None))
+
+        P.pop(0)
+        sorted_headers.remove(least_element)
+
+    adjacency_list[sorted_headers[0]].append((sorted_headers[1], None))
+    adjacency_list[sorted_headers[1]].append((sorted_headers[0], None))
+
+    return adjacency_list
+
+
 if __name__ == "__main__":
     tree_path = "../output/prufer-test.json"  # Path to the tree JSON file
-    P, headers_sorted = prufer_encoding(tree_path)
+    P, sorted_headers = prufer_encoding(tree_path)
+    print("======================================")
     print(f"Prufer Encoding: {P}")
-    print(f"Sorted Headers (Nodes): {headers_sorted}")
+    print(f"Sorted Headers (Nodes): {sorted_headers}")
+    adjacency_list = prufer_decoding(P, sorted_headers)
+    print("======================================")
+    print("Prufer Decoding:")
+    pprint.pprint(adjacency_list)
 
